9ea4fc37e4d462e172a3dbccf61b0db808542123
leiserfg <leiserfg@gmail.com>
2025-03-15 15:37:18 +0100

    Clean optional code

--- src/inspector.rs
+++ src/inspector.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read, os::fd::AsRawFd};
+use std::{fs::File, io::Read};
 
 #[derive(Debug, PartialEq)]
 pub(crate) enum Background {
@@ -9,8 +9,8 @@ use termion::{get_tty, raw::IntoRawMode};
 
 #[cfg(target_os = "linux")]
 fn is_linux_console(tty: &File) -> bool {
+    use std::os::fd::AsRawFd;
     use std::os::raw::c_ulong;
-
     let mode = 0usize; // We won't use the mode but we still need to pass it to ioctl
     const KDGETMODE: c_ulong = 0x4B3B;
 
@@ -18,7 +18,7 @@ fn is_linux_console(tty: &File) -> bool {
 }
 
 #[cfg(not(target_os = "linux"))]
-fn is_linux_console(tty: &File) -> bool {
+fn is_linux_console(_tty: &File) -> bool {
     false
 }
 
