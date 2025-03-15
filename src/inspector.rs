cc48c9faeb1017750eb197552355601d3ae0ae2c
leiserfg <leiserfg@gmail.com>
2025-03-15 15:13:31 +0100

    Add workaround for ghostty and linux console

--- src/inspector.rs
+++ src/inspector.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Borrow, io::Read, io::Write};
+use std::{fs::File, io::Read, os::fd::AsRawFd};
 
 #[derive(Debug, PartialEq)]
 pub(crate) enum Background {
@@ -7,25 +7,68 @@ pub(crate) enum Background {
 }
 use termion::{get_tty, raw::IntoRawMode};
 
-pub(crate) fn probe() -> Option<Background> {
-    let mut tty = get_tty().expect("We need a tty to do the check");
-    let _ = tty.write(b"\x1b[?996n\x1b[c").expect("What happened");
-    let tty_raw = tty.borrow().into_raw_mode().expect("Can't use raw");
+fn is_linux_console(tty: &File) -> bool {
+    let mode = 0usize; // We won't use the mode but we still need to pass it to ioctl
+    const KDGETMODE: libc::Ioctl = 0x4B3B;
 
-    // We store only the first 8 characters, as they will contain the response
-    // to 996 if the terminal suports the protocol correctly, then we keep reading until c
-    // that marks the end of the response to [c
+    unsafe { libc::ioctl(tty.as_raw_fd(), KDGETMODE, &mode) == 0 }
+}
+
+fn is_ghostty<Tty>(tty: &mut Tty) -> bool
+where
+    Tty: IntoRawMode + Read,
+{
+    let _ = tty.write(b"\x1b[>0q\x1b[c").expect("Din't allow the write");
+
+    let ghostty_fingerprint = b"\x1bP>|ghostty";
+
+    let mut buff: Vec<u8> = Vec::with_capacity(ghostty_fingerprint.len());
+
+    {
+        let mut tty_raw = tty.into_raw_mode().expect("Can't use raw");
+
+        for b in tty_raw.bytes() {
+            let c = b.expect("Can't read");
+            if c == b'[' {
+                break;
+            }
+            if buff.len() < ghostty_fingerprint.len() {
+                buff.push(c);
+            }
+        }
+        for b in tty_raw.bytes() {
+            let c = b.expect("Can't read");
+            if c == b'c' {
+                break;
+            }
+        }
+    };
+    buff.as_slice() == ghostty_fingerprint
+}
+
+fn get_bg_ghostty<Tty>(tty: &mut Tty) -> Option<Background>
+where
+    Tty: IntoRawMode + Read,
+{
+    let _ = tty.write(b"\x1b[?996n").expect("What happened");
+
+    // Similar to the case when using the CSI [c guard, but here we know we are in ghostty that is
+    // able to report dark/light but has an issue with the order ofthe csi messages
+    // So we can workarounding by sending only one message and reading til `n`
     let mut to_read = 8;
     let mut buff: Vec<u8> = Vec::with_capacity(to_read);
 
-    for b in tty_raw.bytes() {
-        let c = b.expect("Can't read");
-        if to_read > 0 {
-            buff.push(c);
-            to_read -= 1;
-        }
-        if c == b'c' {
-            break;
+    {
+        let mut tty_raw = tty.into_raw_mode().expect("Can't use raw");
+        for b in tty_raw.bytes() {
+            let c = b.expect("Can't read");
+            if to_read > 0 {
+                buff.push(c);
+                to_read -= 1;
+            }
+            if c == b'n' {
+                break;
+            }
         }
     }
 
@@ -34,4 +77,48 @@ pub(crate) fn probe() -> Option<Background> {
         b"\x1b[?997;2" => Some(Background::Light),
         _ => None,
     }
+}
+fn get_bg_using_guard<Tty>(tty: &mut Tty) -> Option<Background>
+where
+    Tty: IntoRawMode + Read,
+{
+    let _ = tty.write(b"\x1b[?996n\x1b[c").expect("What happened");
+
+    // We store only the first 8 characters, as they will contain the response
+    // to 996 if the terminal suports the protocol correctly, then we keep reading until c
+    // that marks the end of the response to [c
+    let mut to_read = 8;
+    let mut buff: Vec<u8> = Vec::with_capacity(to_read);
+
+    {
+        let mut tty_raw = tty.into_raw_mode().expect("Can't use raw");
+        for b in tty_raw.bytes() {
+            let c = b.expect("Can't read");
+            if to_read > 0 {
+                buff.push(c);
+                to_read -= 1;
+            }
+            if c == b'c' {
+                break;
+            }
+        }
+    }
+
+    match buff.as_slice() {
+        b"\x1b[?997;1" => Some(Background::Dark),
+        b"\x1b[?997;2" => Some(Background::Light),
+        _ => None,
+    }
+}
+
+pub(crate) fn probe() -> Option<Background> {
+    let mut tty = get_tty().expect("Can't open tty, windows maybe?");
+
+    if is_linux_console(&tty) {
+        return None;
+    }
+    if is_ghostty(&mut tty) {
+        return get_bg_ghostty(&mut tty);
+    }
+    get_bg_using_guard(&mut tty)
 }
