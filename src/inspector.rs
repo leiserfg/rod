baa23e6c056ded538a93985787919ab6c4b18f16
leiserfg <leiserfg@gmail.com>
2025-03-15 10:38:24 +0100

    Fix csi parsing

--- src/inspector.rs
+++ src/inspector.rs
@@ -1,38 +1,37 @@
-use std::{borrow::Borrow, io::Write};
+use std::{borrow::Borrow, io::Read, io::Write};
 
 #[derive(Debug, PartialEq)]
 pub(crate) enum Background {
     Dark,
     Light,
 }
-use termion::{event::Key, get_tty, input::TermRead, raw::IntoRawMode};
+use termion::{get_tty, raw::IntoRawMode};
 
 pub(crate) fn probe() -> Option<Background> {
     let mut tty = get_tty().expect("We need a tty to do the check");
     let _ = tty.write(b"\x1b[?996n\x1b[c").expect("What happened");
     let tty_raw = tty.borrow().into_raw_mode().expect("Can't use raw");
 
-    // We need to read char by char cause the message does not end in \n
-    let mut buff = String::new();
+    // We store only the first 8 characters, as they will contain the response
+    // to 996 if the terminal suports the protocol correctly, then we keep reading until c
+    // that marks the end of the response to [c
+    let mut to_read = 8;
+    let mut buff: Vec<u8> = Vec::with_capacity(to_read);
 
-    let mut to_read = 8usize;
-    buff.reserve(to_read);
-
-    for b in tty_raw.keys() {
-        if let Key::Char(c) = b.expect("Error reading") {
-            if c == 'c' {
-                break;
-            }
-            if to_read > 0 {
-                buff.push(c);
-                to_read -= 1;
-            }
+    for b in tty_raw.bytes() {
+        let c = b.expect("Can't read");
+        if to_read > 0 {
+            buff.push(c);
+            to_read -= 1;
+        }
+        if c == b'c' {
+            break;
         }
     }
 
-    match buff.as_str() {
-        "997;1n62" => Some(Background::Dark),
-        "997;2n62" => Some(Background::Light),
+    match buff.as_slice() {
+        b"\x1b[?997;1" => Some(Background::Dark),
+        b"\x1b[?997;2" => Some(Background::Light),
         _ => None,
     }
 }
